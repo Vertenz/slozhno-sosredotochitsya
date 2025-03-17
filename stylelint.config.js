a61a5bf508ac0c60258812091d4fc349183b46c5
Vladimir Vertenz <vladmirworld@gmail.com>
2025-03-17 17:45:59 +0300

    fix: адаптив, линтер

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-html'],
-  plugins: ['stylelint-use-logical'],
-  rules: {
-    'csstools/use-logical': 'always',
-    'value-keyword-case': ['lower', { camelCaseSvgKeywords: true }],
-    'font-family-name-quotes': 'always-where-recommended',
-    'function-url-quotes': 'never',
-    'selector-class-pattern': null,
-  },
-  overrides: [
-    {
-      files: ['*.html', '**/*.html'],
-      customSyntax: 'postcss-html',
+    extends: ['stylelint-config-standard', 'stylelint-config-html'],
+    plugins: ['stylelint-use-logical'],
+    rules: {
+      'csstools/use-logical': 'always',
+      'value-keyword-case': ['lower', { camelCaseSvgKeywords: true }],
+      'font-family-name-quotes': 'always-where-recommended',
+      'function-url-quotes': 'never',
+      'selector-class-pattern': null,
     },
-  ],
-};
+    overrides: [
+      {
+        files: ['*.html', '**/*.html'],
+        customSyntax: 'postcss-html',
+      },
+    ],
+  };
